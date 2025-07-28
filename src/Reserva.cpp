daf88b94004929e3a8740c6cedc7c51ea7833fd8
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-07-27 22:40:32 -0500

    setters Reserva

--- src/Reserva.cpp
+++ src/Reserva.cpp
@@ -8,3 +8,26 @@ void Reserva::validarFechas() const {
 		throw std::invalid_argument("Error: La fecha de salida debe ser posterior a la de entrada");
 	}
 }
+
+//Constructor
+Reserva::Reserva(int id, int id_habitacion, const std::string cliente,
+	const std::chrono::year_month_day& fecha_entrada,
+	const std::chrono::year_month_day& fecha_salida,
+	const std::string& estado)
+	: id_(id), id_habitacion_(id_habitacion), cliente_(cliente),
+	  fecha_entrada_(fecha_entrada), fecha_salida_(fecha_salida), estado_(estado) {
+	validarFechas();
+}
+
+//Getters
+int Reserva::getId() const noexcept { return id_; }
+int Reserva::getHabitacionId() const noexcept { return id_habitacion_; }
+const std::string& Reserva::getCliente() const noexcept { return cliente_; }
+const std::string& Reserva::getEstado() const noexcept { return estado_; }
+const std::chrono::year_month_day& Reserva::getFechaEntrada() const noexcept { return fecha_entrada_; }
+const std::chrono::year_month_day& Reserva::getFechaSalida() const noexcept { return fecha_salida_; }
+
+//Setters
+void Reserva::setEstado(std::string estado) {
+	estado_ = std::move(estado);
+}
